a10469fb77166f150d35eb184aa85e48f8ea793a
Vladislav <doomgar5989@mail.ru>
2025-02-16 18:32:59 +0300

    lab 2 init

---
+++ LR2/index.js
@@ -0,0 +1,19 @@
+const { DateTime } = require(`luxon`)
+
+function timeUntilNewYear() {
+	const now = DateTime.now().setZone(`Europe/Moscow`)
+	const newYear = DateTime.fromObject({ year: now.year + 1, month: 1, day: 1 }, { zone: `Europe/Moscow` })
+
+	const duration = newYear.diff(now, [`days`, `hours`, `minutes`, `seconds`]).toObject()
+
+	const units = {
+		d: duration.days,
+		h: duration.hours,
+		m: duration.minutes,
+		s: Math.floor(duration.seconds)
+	}
+
+	console.log(`До нового года осталось: ${units.d} дней, ${units.h} часов, ${units.m} минут, ${units.s} секунд 🎉`)
+}
+
+timeUntilNewYear()
